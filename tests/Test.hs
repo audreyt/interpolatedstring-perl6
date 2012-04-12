9249f390e7f30e08100b67c566f814837201809c
Adam Curtis <acurtis@spsu.edu>
2012-04-12 15:58:41 -0400

    add support for Text and ByteString interpolation

--- tests/Test.hs
+++ tests/Test.hs
@@ -4,6 +4,9 @@ module Main where
 
 import Text.InterpolatedString.Perl6
 import Test.HUnit
+import GHC.Exts(fromString)
+import Data.ByteString.Char8 (ByteString)
+import Data.Text (Text)
 
 data Foo = Foo Int String deriving Show
 
@@ -22,6 +25,9 @@ testComplex     = assertBool "" ([$qc|
     "        ok\n" ++
     "[Foo 4 \"Great!\",Foo 3 \"Scott!\"]\n" ++
     "        then\n"))
+testConvert = assertBool "" 
+              (([$qc|{fromString "a"::Text} {fromString "b"::ByteString}|] :: String)
+               == "a b")
 
 
 tests = TestList
@@ -31,6 +37,7 @@ tests = TestList
     , TestLabel "Dollar Variable" $ TestCase testVariable
     , TestLabel "Escape Sequences" $ TestCase testEscape
     , TestLabel "Complex Expression" $ TestCase testComplex
+    , TestLabel "String Conversion" $ TestCase testConvert
     ]
 
 main = runTestTT tests
